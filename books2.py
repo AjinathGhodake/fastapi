21915369988cfe02f6f6774403dfc0c2974044d9
Ajinath Ghodake <avin2221992@gmail.com>
2024-07-14 12:42:41 +0530

    exception handling

--- books2.py
+++ books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
 from starlette import status
 
@@ -68,6 +68,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(detail="Book Not Found", status_code=404)
 
 
 @app.get("/books/")
@@ -103,14 +104,22 @@ def find_book_id(book: Book):
 
 @app.put("/books/update_book")
 async def update_book(book_request: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_request.id:
             BOOKS[i] = book_request
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Book id not Found!")
 
 
 @app.delete("/books/{book_id}")
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
+            book_changed = True
             break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail="Book id not Found!")
